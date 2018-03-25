46d760f12c3e3dffabd1b118e6eadeecfac7e782
Analia Mok <axm2580@g.rit.edu>
2018-03-25 14:11:19 -0400

    Added custom slideshow styling and script interaction

--- themes/main-theme/assets/js/src/main.js
+++ themes/main-theme/assets/js/src/main.js
@@ -86,4 +86,81 @@
     }
   }
 
+  // Project Carousel
+
+  if($('.project-carousel').length != 0){
+    // Switch current active image based on thumbnail click
+    $('.project-carousel__image-container').click(changeCurrentImage);
+
+    // Switch current active image based on arrows
+    $('.project-carousel__active__left-arrow').click(prevProjectImage);
+    $('.project-carousel__active__right-arrow').click(nextProjectImage);
+  }
+
+  function changeCurrentImage(e){
+    var target = e.target,
+        targetId = target.getAttribute('data-id');
+    var image = target.querySelector('img'),
+        imageSrc = image.src;
+    
+    // Change current image
+    var activeItem = $('.project-carousel__active'),
+        activeImage = activeItem.find('img');
+    
+    activeImage[0].src = imageSrc;
+    activeItem[0].setAttribute('data-id', targetId);
+
+    // Swap which thumbnail has the active-thumbnail class
+    $('.project-carousel__active-thumbnail').removeClass('project-carousel__active-thumbnail');
+    target.classList.add('project-carousel__active-thumbnail');
+  }
+
+  function prevProjectImage(){
+    var activeItem = $('.project-carousel__active'),
+        activeImage = activeItem.find('img'),
+        currentSlide = parseInt(activeItem.attr('data-id')),
+        totalSlides = parseInt($('.project-carousel').attr('data-total'));
+
+    var prevSlideNumber;
+    if(currentSlide == 1){
+      prevSlideNumber = totalSlides;
+    }else{
+      prevSlideNumber = currentSlide - 1;
+    }
+
+    var prevImageItem = $('#image' + prevSlideNumber),
+        prevImageSrc = prevImageItem[0].src;
+
+    activeImage[0].src = prevImageSrc;
+    activeItem[0].setAttribute('data-id', prevSlideNumber);
+
+    // Swap which thumbnail has the active-thumbnail class
+    $('.project-carousel__active-thumbnail').removeClass('project-carousel__active-thumbnail');
+    prevImageItem[0].parentElement.classList.add('project-carousel__active-thumbnail');
+  }
+
+  function nextProjectImage(){
+    var activeItem = $('.project-carousel__active'),
+        activeImage = activeItem.find('img'),
+        currentSlide = parseInt(activeItem.attr('data-id')),
+        totalSlides = parseInt($('.project-carousel').attr('data-total'));
+
+    var nextSlideNumber;
+    if(currentSlide == totalSlides){
+      nextSlideNumber = 1;
+    }else{
+      nextSlideNumber = currentSlide + 1;
+    }
+    
+    var nextImageItem = $('#image' + nextSlideNumber),
+        nextImageSrc = nextImageItem[0].src;
+
+    activeImage[0].src = nextImageSrc;
+    activeItem[0].setAttribute('data-id', nextSlideNumber);
+
+    // Swap which thumbnail has the active-thumbnail class
+    $('.project-carousel__active-thumbnail').removeClass('project-carousel__active-thumbnail');
+    nextImageItem[0].parentElement.classList.add('project-carousel__active-thumbnail');
+  }
+
 })(jQuery);
